fdb3d85869b839da60916dd0ca64510883dabb17
minhnv <minhnv@kaopiz.com>
2024-05-31 15:43:24 +0700

    first init

---
+++ Source/SwordAndShield/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SWORDANDSHIELD_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
